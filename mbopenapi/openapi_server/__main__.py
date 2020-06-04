ac57993d3af1fb71faf3e547cbefbb24dd418f37
artlands <lijie0416@gmail.com>
2020-06-04 18:36:20 -0500

    add filters

--- mbopenapi/openapi_server/__main__.py
+++ mbopenapi/openapi_server/__main__.py
@@ -11,7 +11,7 @@ def main():
     app.add_api('openapi.yaml',
                 arguments={'title': 'MetricsBuilder API'},
                 pythonic_params=True)
-    app.run(port=8090)
+    app.run(port=8080)
 
 
 if __name__ == '__main__':
